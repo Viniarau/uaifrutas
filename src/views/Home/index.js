e8e0e58876fe5b811203628ef5bb3a0feffec9a9
Viniarau <vinicius.j.s.a22@hotmail.com>
2021-09-20 23:13:23 -0300

    correção saida

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -8,18 +8,18 @@ import {
   Modal,
   TouchableOpacity,
   Pressable,
-} from 'react-native';
-import CardProduct from '../../components/CardProduct';
-import CardCart from '../../components/CardCarrinho';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+} from "react-native";
+import CardProduct from "../../components/CardProduct";
+import CardCart from "../../components/CardCarrinho";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   faShoppingCart,
   faCheckCircle,
   faFilePdf,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 
-import {LAYOUT} from '../../constants/app';
-import {styles} from './styles';
+import { LAYOUT } from "../../constants/app";
+import { styles } from "./styles";
 
 class HomeView extends Component {
   constructor(props) {
@@ -29,66 +29,66 @@ class HomeView extends Component {
       store: [
         {
           id: 1,
-          name: 'Morango',
-          price: '4,98',
-          type: 'bandeja',
-          image: require('../../assets/img/morango.jpg'),
+          name: "Morango",
+          price: "4,98",
+          type: "bandeja",
+          image: require("../../assets/img/morango.jpg"),
         },
         {
           id: 2,
-          name: 'Abacate',
-          price: '9,90',
-          type: 'kg',
-          image: require('../../assets/img/abacate.jpg'),
+          name: "Abacate",
+          price: "9,90",
+          type: "kg",
+          image: require("../../assets/img/abacate.jpg"),
         },
         {
           id: 3,
-          name: 'Banana',
-          price: '2,98',
-          type: 'kg',
-          image: require('../../assets/img/banana-caturra.jpg'),
+          name: "Banana",
+          price: "2,98",
+          type: "kg",
+          image: require("../../assets/img/banana-caturra.jpg"),
         },
         {
           id: 4,
-          name: 'Laranja',
-          price: '3,98',
-          type: 'kg',
-          image: require('../../assets/img/laranja.jpg'),
+          name: "Laranja",
+          price: "3,98",
+          type: "kg",
+          image: require("../../assets/img/laranja.jpg"),
         },
         {
           id: 5,
-          name: 'Limão',
-          price: '5,98',
-          type: 'kg',
-          image: require('../../assets/img/limao.jpg'),
+          name: "Limão",
+          price: "5,98",
+          type: "kg",
+          image: require("../../assets/img/limao.jpg"),
         },
         {
           id: 6,
-          name: 'Maçã',
-          price: '7,98',
-          type: 'kg',
-          image: require('../../assets/img/maca.jpg'),
+          name: "Maçã",
+          price: "7,98",
+          type: "kg",
+          image: require("../../assets/img/maca.jpg"),
         },
         {
           id: 7,
-          name: 'Mamão',
-          price: '2,98',
-          type: 'kg',
-          image: require('../../assets/img/mamao-papaia.jpg'),
+          name: "Mamão",
+          price: "2,98",
+          type: "kg",
+          image: require("../../assets/img/mamao-papaia.jpg"),
         },
         {
           id: 8,
-          name: 'Tangerina',
-          price: '3,98',
-          type: 'kg',
-          image: require('../../assets/img/tangerina.jpg'),
+          name: "Tangerina",
+          price: "3,98",
+          type: "kg",
+          image: require("../../assets/img/tangerina.jpg"),
         },
         {
           id: 9,
-          name: 'Lichia',
-          price: '12,98',
-          type: 'kg',
-          image: require('../../assets/img/lichia.jpg'),
+          name: "Lichia",
+          price: "12,98",
+          type: "kg",
+          image: require("../../assets/img/lichia.jpg"),
         },
       ],
       carrinho: [],
@@ -101,13 +101,13 @@ class HomeView extends Component {
   }
 
   _onChangeFormField = (name, value) => {
-    let formEdit = {...this.state.carrinho};
+    let formEdit = { ...this.state.carrinho };
     formEdit[name] = value;
-    this.setState({carrinho: formEdit});
+    this.setState({ carrinho: formEdit });
   };
 
   add = (number, item) => {
-    this.setState({pedidoFinalizado: false});
+    this.setState({ pedidoFinalizado: false });
     const obj = {
       id: item.id,
       name: item.name,
@@ -129,7 +129,7 @@ class HomeView extends Component {
             this.state.carrinho.splice(i, 1);
           } else {
             this.state.carrinho[i].qntd = obj.qntd;
-            this.setState({achou: true});
+            this.setState({ achou: true });
           }
           break;
         } else if (i === this.state.carrinho.length - 1) {
@@ -148,7 +148,7 @@ class HomeView extends Component {
     return (
       <View style={styles.container}>
         <StatusBar
-          barStyle="light-content"
+          barStyle='light-content'
           backgroundColor={LAYOUT.COLORS.primary}
           translucent={false}
         />
@@ -169,39 +169,42 @@ class HomeView extends Component {
                 );
               })
             ) : (
-              <View style={{flex: 1, alignItems: 'center'}}>
+              <View style={{ flex: 1, alignItems: "center" }}>
                 <Text>Sem dados</Text>
               </View>
             )}
           </View>
         </ScrollView>
         <TouchableOpacity
-          onPress={() => this.setState({modalVisible: true})}
+          onPress={() => this.setState({ modalVisible: true })}
           style={styles.containerCarrinho}
-          activeOpacity={1}>
+          activeOpacity={1}
+        >
           <FontAwesomeIcon
             icon={faShoppingCart}
             size={this.state.clickedCart ? 40 : 25}
-            color={'#fff'}
+            color={"#fff"}
           />
         </TouchableOpacity>
         <Modal
-          animationType="slide"
+          animationType='slide'
           transparent={true}
           visible={this.state.modalVisible}
           onRequestClose={() => {
-            this.setState({modalVisible: !this.state.modalVisible});
-          }}>
+            this.setState({ modalVisible: !this.state.modalVisible });
+          }}
+        >
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
               <View style={styles.containerModal}>
                 <View style={styles.containerBody}>
                   <View style={styles.titleModal}>
-                    <Text>{''}</Text>
+                    <Text>{""}</Text>
                     <Text>CARRINHO DE COMPRAS</Text>
                     <Pressable
                       style={styles.exit}
-                      onPress={() => this.setState({modalVisible: false})}>
+                      onPress={() => this.setState({ modalVisible: false })}
+                    >
                       <Text style={styles.textExit}>x</Text>
                     </Pressable>
                   </View>
@@ -219,7 +222,7 @@ class HomeView extends Component {
                         );
                       })
                     ) : (
-                      <View style={{flex: 1, alignItems: 'center'}}>
+                      <View style={{ flex: 1, alignItems: "center" }}>
                         <Text>Sem dados</Text>
                       </View>
                     )}
@@ -232,7 +235,8 @@ class HomeView extends Component {
                       modalFinishVisible: !this.state.modalFinishVisible,
                     })
                   }
-                  style={styles.buttonBuy}>
+                  style={styles.buttonBuy}
+                >
                   <Text style={styles.textBuy}>Finalizar Pedido</Text>
                 </TouchableOpacity>
               </View>
@@ -240,34 +244,35 @@ class HomeView extends Component {
           </View>
         </Modal>
         <Modal
-          animationType="slide"
+          animationType='slide'
           transparent={true}
           visible={this.state.modalFinishVisible}
           onRequestClose={() => {
-            this.setState({modalFinishVisible: !this.state.modalFinishVisible});
-          }}>
+            this.setState({
+              modalFinishVisible: !this.state.modalFinishVisible,
+            });
+          }}
+        >
           <View style={styles.centeredView}>
-            <Pressable
-              style={styles.exit}
-              onPress={() => {
-                this.setState({
-                  modalFinishVisible: false,
-                  pedidoFinalizado: true,
-                });
-              }}>
-              <Text style={styles.textExit}>x</Text>
-            </Pressable>
             <View style={styles.modalFinishView}>
               <Text style={styles.textFinish}>PEDIDO EFETUADO</Text>
               <FontAwesomeIcon
                 icon={faCheckCircle}
                 size={200}
-                color={'green'}
+                color={"green"}
               />
-              <View style={styles.buttonPdf}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({
+                    modalFinishVisible: false,
+                    pedidoFinalizado: true,
+                  });
+                }}
+                style={styles.buttonPdf}
+              >
                 <Text style={styles.textPdf}>GERAR COMPROVANTE</Text>
-                <FontAwesomeIcon icon={faFilePdf} size={20} color={'red'} />
-              </View>
+                <FontAwesomeIcon icon={faFilePdf} size={20} color={"red"} />
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
